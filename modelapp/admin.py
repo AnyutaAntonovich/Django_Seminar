a33e656ee41fcd91af5189f8d97053f7c55808e2
AnyutaAntonovich <antonovich_am@bk.ru>
2024-01-13 17:08:17 +0600

    Add adm pan

--- modelapp/admin.py
+++ modelapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+from django.db.models import F
+
+
+@admin.action(description='Up age on 10')
+def up_age(modeladmin, request, queryset):
+    queryset.update(age = 10)
+
+
+@admin.action(description='Up age on 10(2)')
+def up_age2(modeladmin, request, queryset):
+    queryset.update(age = F('age') + 10)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname')
+    list_filter = ('age',)
+    search_fields = ('name', 'surname')
+    fieldsets = (('Blog1', {'fields': ('name', 'surname')}), ('Blog2', {'fields': ('bio', 'age')}),)
+    actions = (up_age, up_age2,)
+
+
+admin.site.register(User, UserAdmin)
+
+
