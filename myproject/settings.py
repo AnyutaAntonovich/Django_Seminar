8779b421aca594df99e5e9fb062f89647a9da8bd
AnyutaAntonovich <antonovich_am@bk.ru>
2024-01-14 15:24:49 +0600

    Add last sem

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1',
                  '37.150.196.123']
 
+INTERNAL_IPS = ['127.0.0.1',]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'modelapp',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
